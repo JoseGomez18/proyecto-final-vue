dbb32c693230a73190483214e579126ea7737a41
SantiagoMadrid <santiagomadrid2020@gmal.com>
2023-11-24 15:55:02 -0500

    primer push

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <template>
+  <NavBar/>
   <HelloWorld />
+  <FootComponet />
 </template>
 
 <script>
@@ -7,12 +9,16 @@ import { defineComponent } from 'vue';
 
 // Components
 import HelloWorld from '../components/HelloWorld.vue';
+import NavBar from '../components/Navbar.vue'
+import FootComponet from '../components/FootComponet.vue'
 
 export default defineComponent({
   name: 'HomeView',
 
   components: {
     HelloWorld,
+    NavBar,
+    FootComponet,
   },
 });
 </script>
